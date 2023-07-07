2bff0f225b83f273c2086df1a182a0f75e348497
Hwasser <victorhwasser@gmail.com>
2023-07-07 09:23:34 +0200

    added more soldier types

--- Backend/models/Army.js
+++ Backend/models/Army.js
@@ -5,6 +5,30 @@ const ArmySchema = new mongoose.Schema({
         type: Number,
         required: true
     },
+    militia: {
+        type: Number,
+        required: false
+    },
+    demolition_maniacs: {
+        type: Number,
+        required: false
+    },
+    gun_nuts: {
+        type: Number,
+        required: false
+    },
+    fortified_trucks: {
+        type: Number,
+        required: false
+    },
+    power_suits: {
+        type: Number,
+        required: false
+    },
+    raiders: {
+        type: Number,
+        required: false
+    },
     owner: {
         type: String,
         required: true
