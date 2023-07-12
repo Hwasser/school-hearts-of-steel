f8e8bdc8ab28338ed0f36a72d3583d26240aca9a
Hwasser <victorhwasser@gmail.com>
2023-07-12 14:52:16 +0200

    started added merge-confirm button

--- Client/src/components/game/GameUI.js
+++ Client/src/components/game/GameUI.js
@@ -1,11 +1,14 @@
 import axios from 'axios';
-import './GameUI.css';
 import Province from './Province';
-import React, { useEffect } from 'react';  
+import React, { useState } from 'react';  
+import './GameUI.css';
 
 export default function GameUI( 
   {onSelectAction, onUpdateArmies, onMergeArmies, names, owners, armies, session, player} ) {
-  const worldSize = 3;
+  
+  const worldSize = 3; // Number of provinces per row
+  const [mergeConfirmation, setMergeConfirmation] = useState(false);
+
 
   function onSelectProvince(index) {
     axios.get('http://localhost:8082/api/provinces/', {
@@ -20,12 +23,12 @@ export default function GameUI(
       console.log(e)
     });
   }
-
+  
   function onSelectArmy(id) {
     if (id == null) {
       return;
     }
-
+    
     axios.get(`http://localhost:8082/api/armies/${id}` )
     .then( (res) => {
       onSelectAction(res.data);
@@ -34,38 +37,53 @@ export default function GameUI(
       console.log(e)
     });
   }
+  
+  function handleMergeArmies(army1, army2, id) {
+    setMergeConfirmation(true);
+    console.log("MERGING BROOAS!");
+    //onMergeArmies(army1, army2, id);
+  }
 
   function handleMoveArmy(fromProvince, toProvince, army, fromSlot) {
-    if (fromProvince == toProvince) {
-      return;
-    }
-
-    // Check if destination province is neightbour from this province
-    const move = fromProvince - toProvince; 
-    if (Math.abs(move) == worldSize 
-      || (move == -1 &&  (fromProvince % worldSize != worldSize-1))
-      || (move == 1 && (fromProvince % worldSize != 0)) ) {
-        // Check if the destination province is ours or belongs to another player
-        if (owners[toProvince] == owners[fromProvince]) {
-          // Only start moving an army if there are any available army slots in dst!
-          if (armies[0][toProvince] == null 
-            || armies[1][toProvince] == null 
-            || armies[2][toProvince] == null 
-            || armies[3][toProvince] == null) {      
-            console.log("move army " + army + " from province " + fromProvince + " to " + toProvince);
-            onUpdateArmies(fromProvince, toProvince, army, fromSlot, false);
-          } else {
-            console.log("No available army slots in that province!");
-          }
-        // If the province is not ours, attack!
-        } else {
-          console.log("attack with army " + army + " from province " + fromProvince + " to " + toProvince);
-          onUpdateArmies(fromProvince, toProvince, army, fromSlot, true);
-        }
-      } else {
-        console.log("Province is too far away!");
-      }
+  if (fromProvince == toProvince) {
+    return;
   }
+  // Check if destination province is neightbour from this province
+  const move = fromProvince - toProvince; 
+  if (Math.abs(move) == worldSize 
+    || (move == -1 &&  (fromProvince % worldSize != worldSize-1))
+    || (move == 1 && (fromProvince % worldSize != 0)) ) {
+      // Check if the destination province is ours or belongs to another player
+      if (owners[toProvince] == owners[fromProvince]) {
+        // Only start moving an army if there are any available army slots in dst!
+        if (armies[0][toProvince] == null 
+          || armies[1][toProvince] == null 
+          || armies[2][toProvince] == null 
+          || armies[3][toProvince] == null) {      
+          console.log("move army " + army + " from province " + fromProvince + " to " + toProvince);
+          onUpdateArmies(fromProvince, toProvince, army, fromSlot, false);
+        } else {
+          console.log("No available army slots in that province!");
+        }
+      // If the province is not ours, attack!
+      } else {
+        console.log("attack with army " + army + " from province " + fromProvince + " to " + toProvince);
+        onUpdateArmies(fromProvince, toProvince, army, fromSlot, true);
+      }
+    } else {
+      console.log("Province is too far away!");
+    }
+  }
+
+  const MergeConfirmation = () => {
+    return (
+      <div className="mergeConfirmationPopup">
+      <h3>Are you sure you want to merge these armies?</h3>
+      <button>Confirm</button>
+      <button>Cancel</button>
+      </div>
+    );
+  };
 
   function BuildBody() {
       const body = [];
@@ -84,7 +102,7 @@ export default function GameUI(
                 onProvinceClick={ () => onSelectProvince(index) }
                 onArmyClick={onSelectArmy}
                 onMoveArmy={handleMoveArmy}
-                onMergeArmies={onMergeArmies}
+                onMergeArmies={handleMergeArmies}
                 owner={owner}
                 name={name} 
                 armies={provArmies}
@@ -100,12 +118,12 @@ export default function GameUI(
 
   return (
     <>
+    {mergeConfirmation && (
+      <MergeConfirmation />
+    )}
     <div className="main_screen">
         <BuildBody />
     </div>
     </>
   );
 }
-
-
-
