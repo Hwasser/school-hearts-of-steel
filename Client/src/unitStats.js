265e44c96ff8e71195abceaa3aa310ff7d28671b
Hwasser <victorhwasser@gmail.com>
2023-07-14 08:46:21 +0200

    Ive updated the unitStats and added terrain modifiers

--- Client/src/unitStats.js
+++ Client/src/unitStats.js
@@ -6,8 +6,9 @@ const units = {
     militia: {
         type: 'militia',
         name: 'Militia',
-        soft_attack: 10,
-        hard_attack: 1,
+        damage_low: 6,
+        damage_high: 10,
+        piercing: 1,
         hardness: 0,
         hp: 20,
         cost : {
@@ -15,76 +16,194 @@ const units = {
             fuel: 0,
             tools: 1,
             material: 0
+        },
+        attack_mod: {
+            'plain': 1,
+            'forest': 0.90,
+            'mountain': 0.90,
+            'swamp': 0.90,
+            'urban': 0.90
+        },
+        defence_mod: {
+            'plain': 1,
+            'forest': 1.10,
+            'mountain': 1.10,
+            'swamp': 1.10,
+            'urban': 1.10
         }
     },
     demolition_maniac: {
         type: 'demolition_maniac',
         name: 'Demolition Maniac',
-        soft_attack: 8,
-        hard_attack: 8,
+        damage_low: 4,
+        damage_high: 10,
+        piercing: 7,
         hardness: 10,
         hp: 20,
         cost : {
             food: 5,
-            fuel: 3,
+            fuel: 2,
             tools: 2,
             material: 0
+        },
+        attack_mod: {
+            'plain': 1,
+            'forest': 0.90,
+            'mountain': 0.90,
+            'swamp': 0.90,
+            'urban': 0.90
+        },
+        defence_mod: {
+            'plain': 1,
+            'forest': 1.10,
+            'mountain': 1.10,
+            'swamp': 1.10,
+            'urban': 1.10
         }
     },
     gun_nut: {
         type: 'gun_nut',
         name: 'Gun Nut',
-        soft_attack: 15,
-        hard_attack: 2,
+        damage_low: 8,
+        damage_high: 16,
+        piercing: 2,
         hardness: 10,
         hp: 20,
         cost : {
             food: 5,
             fuel: 0,
-            tools: 5,
+            tools: 4,
             material: 0
+        },
+        attack_mod: {
+            'plain': 1.10,
+            'forest': 0.90,
+            'mountain': 0.90,
+            'swamp': 0.90,
+            'urban': 0.90
+        },
+        defence_mod: {
+            'plain': 1.10,
+            'forest': 1.10,
+            'mountain': 1.10,
+            'swamp': 1.10,
+            'urban': 1.10
         }
     },
     fortified_truck: {
         type: 'fortified_truck',
         name: 'Fortified Truck',
-        soft_attack: 12,
-        hard_attack: 4,
-        hardness: 60,
-        hp: 30,
+        damage_low: 6,
+        damage_high: 14,
+        piercing: 4,
+        hardness: 50,
+        hp: 40,
         cost : {
-            food: 5,
-            fuel: 10,
-            tools: 3,
-            material: 5
+            food: 6,
+            fuel: 6,
+            tools: 2,
+            material: 2
+        },
+        attack_mod: {
+            'plain': 1.20,
+            'forest': 0.80,
+            'mountain': 0.80,
+            'swamp': 0.60,
+            'urban': 0.80
+        },
+        defence_mod: {
+            'plain': 1,
+            'forest': 0.90,
+            'mountain': 0.90,
+            'swamp': 0.80,
+            'urban': 1
         }
     },
     power_suit: {
         type: 'power_suit',
         name: 'Power Suit',
-        soft_attack: 16,
-        hard_attack: 6,
-        hardness: 90,
-        hp: 25,
+        damage_low: 12,
+        damage_high: 20,
+        piercing: 8,
+        hardness: 80,
+        hp: 30,
         cost : {
-            food: 5,
-            fuel: 5,
-            tools: 6,
-            material: 10
+            food: 6,
+            fuel: 4,
+            tools: 5,
+            material: 5
+        },
+        attack_mod: {
+            'plain': 1,
+            'forest': 0.90,
+            'mountain': 0.90,
+            'swamp': 0.90,
+            'urban': 0.90
+        },
+        defence_mod: {
+            'plain': 1,
+            'forest': 1.10,
+            'mountain': 1.10,
+            'swamp': 1.10,
+            'urban': 1.10
         }
     },
     raider: {
         type: 'raider',
         name: 'Raider',
-        soft_attack: 12,
-        hard_attack: 2,
+        damage_low: 2,
+        damage_high: 12,
+        piercing: 2,
         hardness: 25,
-        hp: 20,
+        hp: 25,
         cost : {
-            food: 2,
+            food: 0,
             fuel: 0,
-            tools: 1,
+            tools: 0,
             material: 0
+        },
+        attack_mod: {
+            'plain': 1,
+            'forest': 1,
+            'mountain': 1,
+            'swamp': 1,
+            'urban': 1
+        },
+        defence_mod: {
+            'plain': 1,
+            'forest': 1.10,
+            'mountain': 1.10,
+            'swamp': 1.10,
+            'urban': 1.20
+        }
+    },
+    mutant: {
+        type: 'raider',
+        name: 'Raider',
+        damage_low: 2,
+        damage_high: 18,
+        piercing: 2,
+        hardness: 40,
+        hp: 40,
+        cost : {
+            food: 0,
+            fuel: 0,
+            tools: 0,
+            material: 0
+        },
+        attack_mod: {
+            'plain': 1,
+            'forest': 1,
+            'mountain': 1,
+            'swamp': 1,
+            'urban': 1
+        },
+        defence_mod: {
+            'plain': 1,
+            'forest': 1.20,
+            'mountain': 1.10,
+            'swamp': 1.20,
+            'urban': 1.10
         }
     }
 };
