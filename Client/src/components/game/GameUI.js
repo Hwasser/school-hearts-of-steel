ee2ca4d3154f191decfb7e3719ca826e1d1873e0
Hwasser <victorhwasser@gmail.com>
2023-08-09 09:16:31 +0200

    broke out army logics

--- Client/src/components/game/GameUI.js
+++ Client/src/components/game/GameUI.js
@@ -9,6 +9,8 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import Province from './Province';
 import './GameUI.css';
+import {setupMovement} 
+    from '../../functionality/armyLogic';
 import {host} from '../../backend_adress';
 
 export default function GameUI( 
@@ -83,48 +85,15 @@ export default function GameUI(
   }
 
 /**
+ * @brief: When dragging an army from one province to another on the screen
  * 
  * @param {Integer} fromProvince: Province number 
  * @param {Integer} toProvince: Province number 
  * @param {JSON} army: The object id (_id) of an army
+ * @param {Integer} fromSlot: Which slot the army came from
  */
   function handleMoveArmy(fromProvince, toProvince, army, fromSlot) {
-  // Check if destination province is neightbour from this province
-  const move = fromProvince - toProvince; 
-  if (Math.abs(move) == worldRowSize 
-    || (move === -1 &&  (fromProvince % worldRowSize !== worldRowSize-1))
-    || (move === 1 && (fromProvince % worldRowSize !== 0) || move === 0) ) {
-      const province1 = provinceId[fromProvince];
-      const province2 = provinceId[toProvince];
-      // Event package to send to server
-      const pendingEventPackage = {
-        type: "movement",
-        provinceID: province1,
-        province2ID: province2,
-        provinceN: fromProvince,
-        province2N: toProvince,
-        army_id: army
-      }
-      // Check if the destination province is ours or belongs to another player
-      if (owners[toProvince] == owners[fromProvince]) {
-        // Only start moving an army if there are any available army slots in dst!
-        if (armies[0][toProvince] == null 
-          || armies[1][toProvince] == null 
-          || armies[2][toProvince] == null 
-          || armies[3][toProvince] == null) {      
-          console.log("move army " + army + " from province " + fromProvince + " to " + toProvince);
-          pushMovements(army, fromProvince, toProvince, fromSlot, pendingEventPackage);
-        } else {
-          console.log("No available army slots in that province!");
-        }
-      // If the province is not ours, attack!
-      } else {
-        console.log("attack with army " + army + " from province " + fromProvince + " to " + toProvince);
-        pushMovements(army, fromProvince, toProvince, fromSlot, pendingEventPackage);
-      }
-    } else {
-      console.log("Province is too far away!");
-    }
+    setupMovement(fromProvince, toProvince, army, fromSlot, provinceId, armies, worldRowSize, owners, pushMovements);
   }
   
   function pushMovements(army, fromProvince, toProvince, fromSlot, pendingEventPackage) {
